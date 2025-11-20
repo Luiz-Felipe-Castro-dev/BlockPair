64b2933599194dfd259b226687e91d3607d9ed1a
Luiz Castro <felipefortescastro@gmail.com>
2025-11-20 02:42:27 -0300

    fixed cors error

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -31,10 +31,7 @@ app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true,
 }));
-app.use(cors({
-    origin: 'https://blockpair.netlify.app/',
-    credentials: true,
-}));
+
 app.use(express.json());
 app.use('/user', UserController)
 app.use('/order', OrderController)
